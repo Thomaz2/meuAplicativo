e16860de88106312013d83724cae6d974a1f2849
Thomaz2 <thomaz.fm02@gmail.com>
2023-10-03 07:37:44 -0300

    teste

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,5 +32,24 @@ class MainActivity : ComponentActivity() {
 @Preview(showSystemUi  = true)
 @Composable
 fun TextoCartao (){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+      Text(
+          text = "Feliz aniversário",
+          fontSize = 40.sp,
+          textAlign = TextAlign.Center
+      )
+
+      Text(
+          text = "João",
+          fontSize = 20.sp
+
+      )
+
+    }
 
 }
