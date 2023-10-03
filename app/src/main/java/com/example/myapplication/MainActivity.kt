c53f504820814fbfe3ee106b0fe0b34ea9952de1
Thomaz2 <thomaz.fm02@gmail.com>
2023-10-03 10:58:53 -0300

    teste1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,18 +3,27 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
@@ -23,7 +32,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-
+                TextoCartao()
             }
         }
     }
@@ -32,24 +41,56 @@ class MainActivity : ComponentActivity() {
 @Preview(showSystemUi  = true)
 @Composable
 fun TextoCartao (){
+
+    Image(
+        painter = painterResource(id = R.drawable.vinte_anos),
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        alpha = 0.5f,
+        modifier = Modifier.fillMaxSize()
+
+    )
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.SpaceEvenly,
         modifier = Modifier.fillMaxSize()
     ) {
 
-      Text(
-          text = "Feliz aniversário ",
-          fontSize = 40.sp,
-          textAlign = TextAlign.Center
-      )
 
-      Text(
-          text = "João",
-          fontSize = 20.sp
+        Text(
+            text = "Feliz aniversário. hghhhhhhhhh",
+            fontSize = 50.sp,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Black,
+            fontFamily = FontFamily.Cursive,
+            color = Color.Red,
+            lineHeight = 80.sp,
+            modifier = Modifier
+                .padding(bottom = 560.dp)
 
-      )
+        )
+        Dedicatoria(nomes = "de: Thomaz")
+
+        Dedicatoria(nomes = "Para: mari")
 
     }
 
+}
+
+@Composable
+fun Dedicatoria(nomes:String){
+
+    Text(
+        text = nomes,
+        fontSize = 30.sp,
+        textAlign = TextAlign.End,
+        fontWeight = FontWeight.Black,
+        fontFamily = FontFamily.Cursive,
+        color = Color.Black,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(end = 50.dp)
+
+
+    )
 }
